e22a638989b81435891a649220ea8417119ed923
kenneth <joshuabecaro@gmail.com>
2026-05-20 13:29:08 +0800

    feat: add initial model downloader script

--- ModelDownloader/Program.cs
+++ ModelDownloader/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Whisper.net.Ggml;
+
+// await using var modelStream = await WhisperGgmlDownloader.Default.GetGgmlModelAsync(GgmlType.LargeV3Turbo, QuantizationType.Q5_0);
+await using var modelStream = await WhisperGgmlDownloader.Default.GetGgmlModelAsync(GgmlType.Tiny);
+await using var fileWriter = File.OpenWrite("tin.bin");
+await modelStream.CopyToAsync(fileWriter);
