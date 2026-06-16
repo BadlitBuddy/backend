d8c03dd3f3f8f86e58eba286cddd37cffe5eb51d
kenneth <joshuabecaro@gmail.com>
2026-06-16 23:30:17 +0800

    refactor: change price field from int to decimal

--- Api.Domain/Entities/SubscriptionPlan.cs
+++ Api.Domain/Entities/SubscriptionPlan.cs
@@ -5,7 +5,7 @@ public class SubscriptionPlan : BaseAuditableEntity<int>
     public required string Name  { get; set; }
     public required string Description  { get; set; }
     
-    public int Price { get; set; }
+    public decimal Price { get; set; }
     public required string Currency { get; set; }
     
     public BillingInterval BillingInterval { get; set; }
