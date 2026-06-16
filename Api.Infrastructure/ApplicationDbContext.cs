9685ec7c8c2b4400a19f7caa6596214f92351d0f
kenneth <joshuabecaro@gmail.com>
2026-06-16 23:55:06 +0800

    feat: add initial app db context

---
+++ Api.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using Api.Application.Common.Interfaces;
+using Api.Infrastructure.Identity;
+
+namespace Api.Infrastructure;
+
+public class ApplicationDbContext : DbContext, IApplicationDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+    
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Organization> Organizations =>  Set<Organization>();
+    public DbSet<OrganizationSubscription> OrganizationSubscriptions =>  Set<OrganizationSubscription>();
+    public DbSet<SubscriptionPlan> SubscriptionPlans =>  Set<SubscriptionPlan>();
+    public DbSet<TranscriptionJob> TranscriptionJobs =>  Set<TranscriptionJob>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<ApplicationUser>().Property(u => u.Id).ValueGeneratedNever();
+        // 1-1 shared PK: AspNetUsers.Id == Users.Id
+        modelBuilder.Entity<ApplicationUser>()
+            .HasOne<User>()
+            .WithOne()
+            .HasForeignKey<ApplicationUser>(a => a.Id)
+            .OnDelete(DeleteBehavior.Cascade);
+        
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
