eecd345f7b54d24b9b8b78b7ba96d47c8cdaff1d
kenneth <joshuabecaro@gmail.com>
2026-06-17 23:29:37 +0800

    feat: add email field

--- Api.Domain/Entities/User.cs
+++ Api.Domain/Entities/User.cs
@@ -7,16 +7,18 @@ public class User : BaseAuditableEntity<Guid>
     private User(){}
 
     [SetsRequiredMembers]
-    public User(Guid userId, Guid publicId, string firstName, string lastName)
+    public User(Guid userId, Guid publicId, string email, string firstName, string lastName)
     {
         Id = userId;
         PublicId = publicId.ToString();
+        Email = email;
         FirstName = firstName;
         LastName = lastName;
     }
     
     public int? OrganizationId { get; private set; }
     public Organization? Organization { get; private set; }
+    public required string Email { get; set; }
     public required string FirstName { get; set; }
     public string? LastName { get; private set; }
     public List<TranscriptionJob> TranscriptionJobs { get; private set; } = [];
