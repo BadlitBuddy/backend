8071ace8fb43ea1ef67f187f4514d94f37917339
kenneth <joshuabecaro@gmail.com>
2026-06-18 23:03:57 +0800

    feat: add identity initial infra

---
+++ Api.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace Api.Infrastructure.Identity;
+
+public class ApplicationUser : IdentityUser<Guid>
+{
+    public required Guid PublicId { get; set; }
+    [MaxLength(250)] public required string FirstName { get; set; }
+    [MaxLength(250)] public string? LastName { get; set; }
+}
---
+++ Api.Infrastructure/Identity/JwtTokenGenerator.cs
@@ -0,0 +1,64 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Api.Application.Common.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
+
+namespace Api.Infrastructure.Identity;
+
+public class JwtTokenGenerator : IJwtTokenGenerator
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly IConfiguration _config;
+
+    public JwtTokenGenerator(UserManager<ApplicationUser> userManager, IConfiguration config)
+    {
+        _userManager = userManager;
+        _config = config;
+    }
+    
+    public async Task<string> CreateAccessTokenAsync(UserClaimsDto userClaimsDto)
+    {
+        var user = await _userManager.FindByIdAsync(userClaimsDto.id.ToString());
+        if (user == null)
+        {
+            throw new UnauthorizedAccessException();
+        }
+        
+        var roles = await _userManager.GetRolesAsync(user);
+
+        var claims = new List<Claim>
+        {
+            new(JwtRegisteredClaimNames.Sub, user.PublicId.ToString()),
+            new(JwtRegisteredClaimNames.Email, user.Email!),
+            new(ClaimTypes.NameIdentifier, user.Id.ToString())
+        };
+
+        claims.AddRange(
+            roles.Select(role =>
+                new Claim(ClaimTypes.Role, role)));
+
+        var key =
+            new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(
+                    _config["Jwt:Key"]!));
+
+        var creds =
+            new SigningCredentials(
+                key,
+                SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: _config["Jwt:Issuer"],
+            audience: _config["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(15),
+            signingCredentials: creds);
+
+        return new JwtSecurityTokenHandler()
+            .WriteToken(token);
+    }
+}
