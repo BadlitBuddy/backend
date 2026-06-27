c1fc81b4dfe32019810f97c57dfc9d7779a2ba96
kenneth <joshuabecaro@gmail.com>
2026-06-27 13:37:45 +0800

    feat: add common extensions in global usings

--- Api.Application/GlobalUsings.cs
+++ Api.Application/GlobalUsings.cs
@@ -3,3 +3,4 @@ global using FluentValidation;
 global using MediatR;
 global using Ardalis.GuardClauses;
 global using Api.Application.Common.Models;
+global using Api.Application.Common.Extensions;
