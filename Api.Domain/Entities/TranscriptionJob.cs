60b4a8bf02b1a3bf57f831eb0008057f2af668e5
kenneth <joshuabecaro@gmail.com>
2026-06-27 13:37:17 +0800

    feat: add methods for status marking

--- Api.Domain/Entities/TranscriptionJob.cs
+++ Api.Domain/Entities/TranscriptionJob.cs
@@ -6,5 +6,35 @@ public class TranscriptionJob : BaseAuditableEntity<int>
     public User? User { get; set; }
     public required string UnprocessedObjectKey { get; set; }
     public string? ProcessedObjectKey { get; set; }
-    public TranscriptionJobStatus JobStatus { get; set; }
+    public TranscriptionJobStatus JobStatus { get; private set; }
+
+    public void MarkAsUploaded()
+    {
+        if (ProcessedObjectKey != null)
+        {
+            throw new InvalidOperationException("Cannot set as uploaded since file is already processed");
+        }
+
+        JobStatus = TranscriptionJobStatus.Uploaded;
+    }
+
+    public void MarkAsProcessing()
+    {
+        if (ProcessedObjectKey != null)
+        {
+            throw new InvalidOperationException("Cannot  set as processing since file is already processed");
+        }
+
+        JobStatus = TranscriptionJobStatus.Processing;
+    }
+
+    public void MarkAsCompleted()
+    {
+        if (ProcessedObjectKey == null)
+        {
+            throw new InvalidOperationException("Cannot set as completed since there is no processed file");
+        }
+
+        JobStatus = TranscriptionJobStatus.Completed;
+    }
 }
