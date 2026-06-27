c6763a196e13c9c04866bd0df7c42bcb907c0e29
kenneth <joshuabecaro@gmail.com>
2026-06-27 18:25:59 +0800

    feat: add event for when file has been uploaded

--- Api.Application/GlobalUsings.cs
+++ Api.Application/GlobalUsings.cs
@@ -4,3 +4,4 @@ global using MediatR;
 global using Ardalis.GuardClauses;
 global using Api.Application.Common.Models;
 global using Api.Application.Common.Extensions;
+global using Api.Domain.Events;
