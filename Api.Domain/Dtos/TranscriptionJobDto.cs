1e10ce6d1bd409ed6f9046bae6d09a8f49997ed9
kenneth <joshuabecaro@gmail.com>
2026-06-29 22:20:58 +0800

    feat: add domain dto for transcription job

---
+++ Api.Domain/Dtos/TranscriptionJobDto.cs
@@ -0,0 +1,9 @@
+namespace Api.Domain.Dtos;
+
+public class TranscriptionJobDto
+{
+    public Guid UserId { get; set; }
+    public required string UnprocessedObjectKey { get; set; }
+    public string? ProcessedObjectKey { get; set; }
+    public TranscriptionJobStatus JobStatus { get; set; }
+}
