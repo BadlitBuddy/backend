7a1db710c91fe2308291b7cc2b21704e3eb32fe2
kenneth <joshuabecaro@gmail.com>
2026-07-12 21:22:22 +0800

    refactor: use pascal case

--- Api.Application/Users/Dtos/UserDetailsDto.cs
+++ Api.Application/Users/Dtos/UserDetailsDto.cs
@@ -2,7 +2,7 @@ namespace Api.Application.Users.Dtos;
 
 public class UserDetailsDto : BaseEntityDto
 {
-    public string Firstname { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
     public string? LastName { get; set; }
     public string Email { get; set; } = string.Empty;
 }
