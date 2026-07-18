28c54593387b7528c6f2d497587b934f89dfa74c
kenneth <joshuabecaro@gmail.com>
2026-07-18 15:47:41 +0800

    feat: add jwtoptions configuration

---
+++ Api.Web/Configuration/JwtOptions.cs
@@ -0,0 +1,10 @@
+namespace Api.Web.Configuration;
+
+public class JwtOptions
+{
+    public string? Key { get; set; }
+    public string? Issuer { get; set; }
+    public string? Audience { get; set; }
+    public int? AccessTokenExpirationMinutes { get; set; }
+    public int? RefreshTokenExpirationDays { get; set; }
+}
