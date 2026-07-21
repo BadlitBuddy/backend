2b52e4f9d30e1f9944ae351dbed41d95d7e1f24b
kenneth <joshuabecaro@gmail.com>
2026-07-21 17:20:07 +0800

    refactor: change transcription proocess mesasge to use JobStatus enum

--- Shared.Contracts/Dtos/MessagePayloadDto.cs
+++ Shared.Contracts/Dtos/MessagePayloadDto.cs
@@ -1,6 +1,8 @@
+using Shared.Contracts.Enums;
+
 namespace Shared.Contracts.Dtos;
 
-public sealed record TranscriptionFinishedMessage(
-    string UserId,
+public sealed record TranscriptionProcessMessage(
+    JobStatus JobStatus,
     string UnprocessedWavFileObjectKey,
     string TranscriptionFileObjectKey);
