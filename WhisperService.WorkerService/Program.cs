4cdebcf080c23de7499d969861a6876c8c848661
kenneth <joshuabecaro@gmail.com>
2026-07-25 21:23:01 +0800

    feat: register cloudflare options and cloudflare http client

--- WhisperService.WorkerService/Program.cs
+++ WhisperService.WorkerService/Program.cs
@@ -1,7 +1,11 @@
+using System.Net.Http.Headers;
+using Microsoft.Extensions.Options;
 using Shared.Abstractions.Jobs;
 using Shared.Abstractions.Services;
 using Shared.Infrastructure;
+using Shared.Infrastructure.Configuration;
 using Shared.Infrastructure.Jobs;
+using Shared.Infrastructure.Services;
 
 var builder = Host.CreateApplicationBuilder(args);
 
@@ -15,7 +19,24 @@ builder.Services
     .AddDapperContext()
     .AddDataRepositories();
 
+builder.Services.Configure<CloudflareOptions>(
+    builder.Configuration.GetSection("Cloudflare")
+);
+
+builder.Services.AddHttpClient<CloudFlareWhisperClient>((sp, client) =>
+{
+    var options = sp.GetRequiredService<IOptions<CloudflareOptions>>().Value;
+    client.BaseAddress = new Uri($"https://api.cloudflare.com/client/v4/accounts/{options.AccountId}/");
+    client.DefaultRequestHeaders.UserAgent.ParseAdd("DotNetApp");
+    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", options.ApiKey);
+}).AddStandardResilienceHandler(options =>
+{
+    options.AttemptTimeout.Timeout = TimeSpan.FromMinutes(5);
+    options.TotalRequestTimeout.Timeout = TimeSpan.FromMinutes(5);
+});
+
 builder.Services.AddSingleton<IStreamingTranscriptionService, WhisperTranscriptionService>();
+builder.Services.AddSingleton<ITranscriptionService, CloudFlareWhisperTranscriptionService>();
 
 builder.Services.AddScoped<ITranscriptionJob, TranscriptionJob>();
 
