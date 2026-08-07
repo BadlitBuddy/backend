240d2e6262fe9606b843f9448b602abde9ad0936
k02_10 <joshuabecaro@gmail.com>
2026-08-07 23:13:03 +0800

    feat: add new duration field for transcription job

---
+++ Api.Infrastructure/Migrations/20260807142806_TranscriptionJob_AddTranscriptionDuration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class TranscriptionJob_AddTranscriptionDuration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "Duration",
+                table: "TranscriptionJobs",
+                type: "interval",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "TranscriptionJobs");
+        }
+    }
+}
