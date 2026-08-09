e4303f982c0804d63a3dc0ceb332d56483f488ce
k02_10 <joshuabecaro@gmail.com>
2026-08-09 17:22:03 +0800

    refactor: remove condiiont to also check for user id

--- Api.Application/TranscriptionJobs/EventHandlers/TranscriptionJobProcessing.cs
+++ Api.Application/TranscriptionJobs/EventHandlers/TranscriptionJobProcessing.cs
@@ -8,23 +8,18 @@ public class TranscriptionJobProcessing : INotificationHandler<TranscriptionJobP
 {
     private readonly IApplicationDbContext _dbContext;
     private readonly IAudioJobStorageService _audioJobStorageService;
-    private readonly IUser _currentUserService;
 
-    public TranscriptionJobProcessing(IApplicationDbContext dbContext, IAudioJobStorageService audioJobStorageService,
-        IUser currentUserService)
+    public TranscriptionJobProcessing(IApplicationDbContext dbContext, IAudioJobStorageService audioJobStorageService)
     {
         _dbContext = dbContext;
         _audioJobStorageService = audioJobStorageService;
-        _currentUserService = currentUserService;
     }
 
     public async Task Handle(TranscriptionJobProcessingEvent notification, CancellationToken cancellationToken)
     {
-        var currentUserId = new Guid(_currentUserService.Id!);
         var transcriptionJob =
             await _dbContext.TranscriptionJobs.SingleOrDefaultAsync(tb =>
-                tb.UnprocessedObjectKey == notification.TranscriptionJob.UnprocessedObjectKey &&
-                tb.UserId == currentUserId, cancellationToken: cancellationToken);
+                tb.PublicId == notification.TranscriptionJob.PublicId, cancellationToken: cancellationToken);
 
         if (transcriptionJob == null)
         {
