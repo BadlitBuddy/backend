191f1ead65d44ebbea0b580d85c2e8c7e878874e
k02_10 <joshuabecaro@gmail.com>
2026-08-17 23:34:05 +0800

    feat!: remove endpoint for updating transcription job status

--- Api.Web/Endpoints/Files.cs
+++ Api.Web/Endpoints/Files.cs
@@ -1,13 +1,7 @@
-using Api.Application.Common.Interfaces;
-using Api.Application.Files.Commands.UpdateFileStatus;
 using Api.Application.Files.Queries.GetUploadPresignedUrl;
 using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.AspNetCore.Mvc;
-using TranscriptionJobStatus = Api.Domain.Enums.TranscriptionJobStatus;
 
 namespace Api.Web.Endpoints;
-
-public record UpdateStatusRequest(string? ProcessedObjectKey, TranscriptionJobStatus TranscriptionJobStatus);
 
 public class Files : IEndpointGroup
 {
@@ -16,27 +10,6 @@ public class Files : IEndpointGroup
         groupBuilder.RequireAuthorization();
 
         groupBuilder.MapPost(Upload).AllowAnonymous();
-        // TODO: This is not  RESTful consolidate under Transcripts
-        groupBuilder.MapPost("status", UpdateStatus);
-    }
-
-    [EndpointSummary("Update Status")]
-    [EndpointDescription("Update the status of the file")]
-    public static async Task<Results<ProblemHttpResult, Ok<UpdateFileStatusResponse>>> UpdateStatus(ISender sender,
-        [FromQuery] string objectKey, UpdateStatusRequest request)
-    {
-        var result = await sender.Send(new UpdateFileStatusCommand
-        {
-            UnprocessedObjectKey = objectKey, ProcessedObjectKey = request.ProcessedObjectKey,
-            TranscriptionJobStatus = request.TranscriptionJobStatus
-        });
-
-        if (result.IsFailure)
-        {
-            return result.ToProblemResult();
-        }
-
-        return TypedResults.Ok(result.Value);
     }
 
     [EndpointSummary("Upload")]
