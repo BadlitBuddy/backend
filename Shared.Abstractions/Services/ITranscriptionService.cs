eb29d6c5c89ce4ec2010c63c054e8aefadd8d8b4
k02_10 <joshuabecaro@gmail.com>
2026-08-18 22:37:06 +0800

    refactor: remove unnecessary methods

--- Shared.Abstractions/Services/ITranscriptionService.cs
+++ Shared.Abstractions/Services/ITranscriptionService.cs
@@ -19,16 +19,6 @@ public interface IStreamingTranscriptionService
 {
     IAsyncEnumerable<TranscriptionSegment> TranscribeStreamingAsync(Stream fileStream,
         CancellationToken cancellationToken);
-
-    // TODO: remove unnecessary methods
-    Task TranscribeAndWriteAsSrtFileAsync(Stream fileStream, string outputFilePath,
-        CancellationToken cancellationToken);
-
-    Task TranscribeAndWriteAsTxtFileAsync(Stream fileStream, string outputFilePath,
-        CancellationToken cancellationToken);
-
-    Task TranscribeAndWriteAsVttFileAsync(Stream fileStream, string outputFilePath,
-        CancellationToken cancellationToken);
 }
 
 public interface ITranscriptionService
