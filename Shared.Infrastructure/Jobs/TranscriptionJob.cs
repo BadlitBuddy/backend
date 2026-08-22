6b2cc6fc97ba516a4707a63b0aecf79651429cf8
k02_10 <joshuabecaro@gmail.com>
2026-08-22 11:43:42 +0800

    feat: add support for large file with groq provider

--- Shared.Infrastructure/Jobs/TranscriptionJob.cs
+++ Shared.Infrastructure/Jobs/TranscriptionJob.cs
@@ -10,12 +10,15 @@ using Shared.Common.Helpers;
 using Shared.Contracts.DtoMappers;
 using Shared.Contracts.Enums;
 using Shared.Infrastructure.Constants;
+using Shared.Infrastructure.Services;
 using TranscriptionJobStatus = Api.Domain.Enums.TranscriptionJobStatus;
 
 namespace Shared.Infrastructure.Jobs;
 
 public class TranscriptionJob : ITranscriptionJob
 {
+    private const long MaxLocalFileSizeToProcess = 24 * 1024 * 1024;
+
     private readonly IStreamingTranscriptionService _transcriptionService;
     private readonly IAudioJobStorageService _audioJobStorageService;
     private readonly ILogger<TranscriptionJob> _logger;
@@ -57,7 +60,7 @@ public class TranscriptionJob : ITranscriptionJob
             throw new InvalidOperationException();
         }
 
-        var (toProcessDir, processedDir) = CreateWorkingDirs();
+        var (toProcessDir, processedDir) = CreateWorkingDirs(fileKey);
         var (toProcessFilePath, processedFilePath) = CreateWorkingFilePaths(toProcessDir, processedDir, fileKey);
         var (userId, originalFileName) = StoragePathBuilder.ExtractUnprocessedFileKeyParts(fileKey);
 
@@ -66,10 +69,7 @@ public class TranscriptionJob : ITranscriptionJob
         {
             _logger.LogInformation("Starting Whisper transcription for: {FileKey}", fileKey);
 
-            await DownloadAndSaveFileAsync(toProcessFilePath, fileKey, cancellationToken);
-
-            await _transcriptRepository.UpdateStatusAsync(fileKey, null,
-                TranscriptionJobStatus.Processing, new Guid(userId));
+            var toProcessFileInfo = await DownloadAndSaveFileAsync(toProcessFilePath, fileKey, cancellationToken);
 
             switch (_workerOptions.TranscriptionProvider)
             {
@@ -78,7 +78,8 @@ public class TranscriptionJob : ITranscriptionJob
                         cancellationToken);
                     break;
                 case TranscriptionProvider.Groq:
-                    await TranscribeWithGroq(processedFilePath, fileKey, cancellationToken);
+                    await TranscribeWithGroq(toProcessFileInfo, processedFilePath, processedDir, fileKey,
+                        cancellationToken);
                     break;
                 case TranscriptionProvider.Cloudflare:
                     await TranscribeWithCloudflare(toProcessFilePath, processedFilePath, fileKey, cancellationToken);
@@ -118,8 +119,19 @@ public class TranscriptionJob : ITranscriptionJob
         }
         finally
         {
-            if (File.Exists(toProcessFilePath)) File.Delete(toProcessFilePath);
-            if (File.Exists(processedFilePath)) File.Delete(processedFilePath);
+            DirectoryInfo directory = new DirectoryInfo(processedDir);
+            if (directory.Exists)
+            {
+                foreach (FileInfo file in directory.EnumerateFiles())
+                {
+                    file.Delete();
+                }
+
+                foreach (DirectoryInfo subDirectory in directory.EnumerateDirectories())
+                {
+                    subDirectory.Delete(recursive: true);
+                }
+            }
 
             await _messagePublisher.PublishAsync(MessageChannel.TranscriptionProcess,
                 new TranscriptionProcessMessage(JobStatus.Finished, fileKey, outputObjectKey));
@@ -135,7 +147,7 @@ public class TranscriptionJob : ITranscriptionJob
             throw new InvalidOperationException();
         }
 
-        var (toProcessDir, processedDir) = CreateWorkingDirs();
+        var (toProcessDir, processedDir) = CreateWorkingDirs(fileKey);
         var (toProcessFilePath, processedFilePath) = CreateWorkingFilePaths(toProcessDir, processedDir, fileKey);
 
         var (userId, originalFileName) = StoragePathBuilder.ExtractUnprocessedFileKeyParts(fileKey);
@@ -224,8 +236,19 @@ public class TranscriptionJob : ITranscriptionJob
         }
         finally
         {
-            if (File.Exists(toProcessFilePath)) File.Delete(toProcessFilePath);
-            if (File.Exists(processedFilePath)) File.Delete(processedFilePath);
+            DirectoryInfo directory = new DirectoryInfo(processedDir);
+            if (directory.Exists)
+            {
+                foreach (FileInfo file in directory.EnumerateFiles())
+                {
+                    file.Delete();
+                }
+
+                foreach (DirectoryInfo subDirectory in directory.EnumerateDirectories())
+                {
+                    subDirectory.Delete(recursive: true);
+                }
+            }
 
             await _messagePublisher.PublishAsync(MessageChannel.TranscriptionProcess,
                 new TranscriptionProcessMessage(JobStatus.Finished, fileKey, outputObjectKey));
@@ -269,45 +292,117 @@ public class TranscriptionJob : ITranscriptionJob
         await _streamableTranscriptionExporter.ToJsonAsync(transcriptionResult, fileStream, cancellationToken);
     }
 
-    private async Task TranscribeWithGroq(string outputFilePath, string fileKey,
-        CancellationToken cancellationToken)
+    private async Task TranscribeWithGroq(FileInfo toProcessFile, string processedFilePath, string processedDir,
+        string fileKey, CancellationToken cancellationToken)
     {
-        if (_hostEnvironment.IsDevelopment())
-        {
-            _logger.LogInformation("Transcribing with Groq");
-        }
+        _logger.LogInformation("Transcribing with Groq");
 
-        var (fileUri, _) =
-            await _audioJobStorageService.CreateDownloadUrlAsync(fileKey, DateTime.UtcNow.AddHours(1),
+        var isLargeFile = toProcessFile.Length > MaxLocalFileSizeToProcess;
+        if (isLargeFile)
+        {
+            var toProcessFileFlac =
+                await AudioFileProcessor.ConvertWavToFlacAsync(toProcessFile, processedDir, 5, cancellationToken);
+
+            const long maxBytesPerChunk = 20 * 1024 * 1024;
+            if (_hostEnvironment.IsDevelopment()) Console.WriteLine($"Max Bytes per chunk {maxBytesPerChunk} bytes");
+            var chunkedFiles = await AudioFileProcessor.ChunkFileAsync(toProcessFileFlac,
+                maxBytesPerChunk,
+                processedDir, 3, cancellationToken);
+
+            if (_hostEnvironment.IsDevelopment())
+            {
+                Array.ForEach(chunkedFiles,
+                    file => _logger.LogInformation("Chunked File {FileName} has size {FileLength} bytes", file.Name,
+                        file.Length));
+            }
+
+            using var progressCts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+            List<Task<TranscriptionResult>> transcriptionTasks = [];
+            foreach (var chunkedFile in chunkedFiles)
+            {
+                _logger.LogInformation($"Transcribing File {chunkedFile.Name}");
+                var transcriptionTask = _groqTranscriptionService.TranscribeAsync(
+                    new TranscriptionSource.FilePath(chunkedFile),
+                    TranscriptionModel.WhisperV3LargeTurbo,
+                    progressCts.Token);
+                transcriptionTasks.Add(transcriptionTask);
+            }
+
+            var tasks = Task.WhenAll(transcriptionTasks);
+            var progressTask = PublishProgressAsync(
+                tasks,
+                fileKey,
+                progressCts.Token);
+
+            TranscriptionResult[] transcriptionResults;
+            try
+            {
+                transcriptionResults = await tasks;
+            }
+            finally
+            {
+                await progressCts.CancelAsync();
+                await progressTask;
+            }
+
+            List<FileInfo> chunkedJsonFiles = [];
+            for (int i = 0; i < transcriptionResults.Length; i++)
+            {
+                string chunkedJsonFilePath = Path.Combine(processedDir, $"chunk-{i}.json");
+                FileInfo chunkedJsonFileInfo = new FileInfo(chunkedJsonFilePath);
+
+                await using var chunkedFileStream = chunkedJsonFileInfo.Create();
+                await _streamableTranscriptionExporter.ToJsonAsync(transcriptionResults[i], chunkedFileStream,
+                    cancellationToken);
+                chunkedJsonFiles.Add(chunkedJsonFileInfo);
+            }
+
+            var mergedJsonTranscriptionResult =
+                await TranscriptionResultMerger.MergeFilesAsync(chunkedJsonFiles, null, cancellationToken);
+
+            await using var mergedFileStream = File.Create(processedFilePath);
+            await _streamableTranscriptionExporter.ToJsonAsync(mergedJsonTranscriptionResult, mergedFileStream,
                 cancellationToken);
 
-        using var progressCts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
-        var transcriptionTask =
-            _groqTranscriptionService.TranscribeAsync(new TranscriptionSource.Url(fileUri),
-                TranscriptionModel.WhisperV3LargeTurbo,
+            if (_hostEnvironment.IsDevelopment())
+            {
+                _logger.LogInformation("Transcribed text: {Text}", mergedJsonTranscriptionResult.Text);
+            }
+        }
+        else
+        {
+            var (fileUri, _) =
+                await _audioJobStorageService.CreateDownloadUrlAsync(fileKey, DateTime.UtcNow.AddHours(1),
+                    cancellationToken);
+
+            using var progressCts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+            var transcriptionTask =
+                _groqTranscriptionService.TranscribeAsync(new TranscriptionSource.Url(fileUri),
+                    TranscriptionModel.WhisperV3LargeTurbo,
+                    progressCts.Token);
+            var progressTask = PublishProgressAsync(
+                transcriptionTask,
+                fileKey,
                 progressCts.Token);
-        var progressTask = PublishProgressAsync(
-            transcriptionTask,
-            fileKey,
-            progressCts.Token);
 
-        TranscriptionResult transcriptionResult;
-        try
-        {
-            transcriptionResult = await transcriptionTask;
-        }
-        finally
-        {
-            await progressCts.CancelAsync();
-            await progressTask;
-        }
+            TranscriptionResult transcriptionResult;
+            try
+            {
+                transcriptionResult = await transcriptionTask;
+            }
+            finally
+            {
+                await progressCts.CancelAsync();
+                await progressTask;
+            }
 
-        await using var fileStream = File.Create(outputFilePath);
-        await _streamableTranscriptionExporter.ToJsonAsync(transcriptionResult, fileStream, cancellationToken);
+            await using var fileStream = File.Create(processedFilePath);
+            await _streamableTranscriptionExporter.ToJsonAsync(transcriptionResult, fileStream, cancellationToken);
 
-        if (_hostEnvironment.IsDevelopment())
-        {
-            _logger.LogInformation("Transcribed text: {Text}", transcriptionResult.Text);
+            if (_hostEnvironment.IsDevelopment())
+            {
+                _logger.LogInformation("Transcribed text: {Text}", transcriptionResult.Text);
+            }
         }
     }
 
@@ -353,7 +448,7 @@ public class TranscriptionJob : ITranscriptionJob
     private async Task<bool> IsFileValid(string unProcessedObjectKey, CancellationToken cancellationToken)
     {
         var isValidFile =
-            await _audioJobStorageService.IsWhisperCompatibleWavAsync(unProcessedObjectKey,
+            await _audioJobStorageService.IsWhisperCompatibleWavAsync(unProcessedObjectKey, null,
                 cancellationToken: cancellationToken);
 
         if (!isValidFile)
@@ -365,10 +460,11 @@ public class TranscriptionJob : ITranscriptionJob
         return true;
     }
 
-    private static (string toProcessDir, string processedDir) CreateWorkingDirs()
+    // *NOTE, using fileKey here to uniquely identify this job
+    private static (string toProcessDir, string processedDir) CreateWorkingDirs(string fileKey)
     {
-        string toProcessDir = Path.Combine(AppContext.BaseDirectory, "MediaFiles", "ToProcess");
-        string processedDir = Path.Combine(AppContext.BaseDirectory, "MediaFiles", "Processed");
+        string toProcessDir = Path.Combine(AppContext.BaseDirectory, "MediaFiles", "ToProcess", fileKey);
+        string processedDir = Path.Combine(AppContext.BaseDirectory, "MediaFiles", "Processed", fileKey);
         Directory.CreateDirectory(toProcessDir);
         Directory.CreateDirectory(processedDir);
 
@@ -385,13 +481,14 @@ public class TranscriptionJob : ITranscriptionJob
         return (toProcessFilePath, processedFilePath);
     }
 
-    private async Task DownloadAndSaveFileAsync(string toProcessFilePath, string unProcessedObjectKey,
+    private async Task<FileInfo> DownloadAndSaveFileAsync(string toProcessFilePath, string unProcessedObjectKey,
         CancellationToken cancellationToken)
     {
         await using var s3Stream =
             await _audioJobStorageService.DownloadAudioAsync(unProcessedObjectKey, cancellationToken);
         await using var fileStream = File.Create(toProcessFilePath);
         await s3Stream.CopyToAsync(fileStream, cancellationToken);
+        return new FileInfo(toProcessFilePath);
     }
 
     private async Task PublishProgressAsync(
